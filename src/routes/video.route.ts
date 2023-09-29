b3925f1ab9947c6244b38644399089a8ccf821b2
raju reddy <raju1997reddy@gmail.com>
2023-09-29 10:05:38 +0530

    videos and stories

--- src/routes/video.route.ts
+++ src/routes/video.route.ts
@@ -1,12 +1,22 @@
 import {videoByIDPlay} from "@/controllers/video.controller";
 import express from "express";
 import multer from "multer";
-import {uploadVideo} from "@/models/video.model";
+import {uploadVideo} from "@/controllers/video.controller";
+//import { Youtube } from "@/controllers/video.controller";
 const router = express.Router();
 
 const storage = multer.memoryStorage();
 const uploadvideo = multer({ storage });
 
+
+
 router.post("/uploadvideo", uploadvideo.single("file") ,uploadVideo);
 
 router.get('/videoplay/:id',videoByIDPlay)
+ 
+import { convertVideoToMP3 } from '@/controllers/video.controller';
+ 
+router.post('/convert', convertVideoToMP3);
+
+
+export default router;
